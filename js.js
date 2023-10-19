a30ef676ef945a183cef078ef49570158f493e1a
stefanige <141761489+stefanige@users.noreply.github.com>
2023-10-19 17:06:27 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,12 @@
+let resp=window.document.getElementById('saida.')
+function functio1(){
+    resp.innerHTML += '<p> clicou no prmeiro botao'
+} 
+function function2(){
+    resp.innerHTML += '<p>clicou no segundo botao'
+
+}
+function function3(){
+    resp.innerHTML += '<p>clicou no terceiro botao'
+
+}
